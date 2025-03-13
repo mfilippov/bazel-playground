8c79a1956d617304f0b2dc9aa5c9eb5020587b16
Mikhail Filippov <mikhail@filippov.me>
2025-03-13 10:08:35 +0100

    add more info

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
   public static void main(String[] args) {
+    String javaVersion = System.getProperty("java.version");
+    String jbrVersion = System.getProperty("java.runtime.version");
+    String osArch = System.getProperty("os.arch", "");
+    String osName = System.getProperty("os.name", "");
+    String vmVendor = System.getProperty("java.vendor", "unknown");
+    String vmVersion = System.getProperty("java.vm.name", "unknown");
+    if (jbrVersion != null) {
+      System.out.println("JVM: " + vmVendor + " " + vmVersion + " " + jbrVersion);
+      System.out.println(" OS: " + osName + " (" + osArch + ")");
+    } else {
+      System.out.println("JVM: " + vmVendor + " " + vmVersion + " " + javaVersion);
+      System.out.println(" OS: " + osName + " (" + osArch + ")");
+    }
     if (args.length > 0) {
       System.out.println("Args:");
       for (String arg : args) {
